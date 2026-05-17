9af8e9529ee5a4a2296d5023d136518a5518d9dd
artajayay-source <artajayay@gmail.com>
2026-05-17 15:16:19 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,8 +8,8 @@ const ASSETS = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 // Install — cache assets
